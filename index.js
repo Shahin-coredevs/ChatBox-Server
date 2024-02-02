97f62b1c18903b4079234e4f345be46b77e4c97d
Shahin-coredevs <shahin_alam@coredevs.ltd>
2024-02-02 19:04:09 +0600

    jwt

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const { createServer } = require('http');
 const { Server } = require('socket.io');
 const cors = require('cors');
-const allMsg = []
-
+const jwt = require('jsonwebtoken');
+const bodyParser = require('body-parser')
+const allUser = []
 const app = express();
 const server = createServer(app);
 const io = new Server(server, {
@@ -13,6 +15,16 @@ const io = new Server(server, {
   }
 });
 
+// Middleware
+function auth (req, res, next){
+  const token = req.headers.cookie.split('=')[1];
+  
+  console.log(token);
+  // console.log(req.headers.cookie);
+  return next()
+}
+
+app.use(bodyParser.json())
 app.use(
   cors({
     origin: ["http://localhost:5173"],
@@ -20,9 +32,48 @@ app.use(
   })
 );
 
+const uri = "mongodb+srv://shahinalam:y89KSJmEgt0LnzUo@cluster0.cuy74ex.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+const userCollection = client.db("ChatApplication").collection("Users");
+async function run() {
+  try {
+    
+    
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+   
+  }
+}
+run().catch(console.dir);
+
 app.get('/', (req, res) => {
   res.send('<h1>Hello world</h1>');
 });
+
+// Login Route
+app.post('/login', async (req, res)=> {
+  const {email, password} = req.body;
+
+  const isValid = await userCollection.findOne({email: email});
+  if(!isValid) return res.status(404).send("User Not Found");
+  if(isValid.password !== password) return res.status(401).send("User Unauthorised");
+  const token = jwt.sign(email, 'itsSecret');
+
+  res.cookie('token', token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: 'none'
+  });
+  return res.status(200).send('Login Success')
+  // console.log(isValidPass);
+  console.log(req.body);
+})
 var roomno = 1;
 io.on('connection', (socket) => {
   console.log(`a user connected ${socket.id}`);
@@ -44,6 +95,35 @@ io.on('connection', (socket) => {
    })
 });
 
+app.post('/users',async (req, res) => {
+  const user = req.body
+  const id= Math.floor((Math.random() * 100000))
+  const userWithId = {...user,id}
+  const result = await userCollection.insertOne(userWithId)
+  res.status(200).send(result)
+})
+
+app.get('/users', auth, async (req,res)=>{
+  const result = await userCollection.find().toArray() 
+  res.status(200).send(result)
+})
+app.get('/users/:email', async (req,res)=>{
+  const Email = req.params.email
+  const result = await userCollection.findOne({email:Email})
+  if(result){
+    const token = jwt.sign(result.email, 'gthryu56hb45gftgfvr@3fdsfvdgrthbrtf15653246t523edfsgtfhtrfderweevtyrwqe', {
+      expiresIn: '1h',
+      });
+  }
+  res.status(200).send(result).cookie('token', token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: 'none'
+})
+})
+
+
+
 server.listen(3000, () => {
   console.log('listening on *:3000');
-});
+}); 
